e13c325bf7619a8fe2c1472073543c438ef50840
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2023-09-18 21:37:49 +0200

    minor fixes, miro's feedbacks

--- frontend/src/Components/Footer/SocialHandleGrid.jsx
+++ frontend/src/Components/Footer/SocialHandleGrid.jsx
@@ -1,15 +1,25 @@
 import { Facebook, LinkedIn, Instagram, Twitter } from '@mui/icons-material/';
+import { AiFillInstagram } from 'react-icons/ai';
 import { Stack } from '@mui/material';
 import CustomLink from '../CustomLink';
 
 export default function SocialHandleGrid() {
   return (
     <Stack direction="column" textAlign="center">
-      <Stack direction="row" spacing={1} justifyContent="center">
-        <CustomLink href="https://twitter.com/cities_nyuad/" text={<Twitter sx={{ fontSize: '2rem' }} />} />
-        <CustomLink href="https://www.linkedin.com/company/center-for-interacting-urban-networks/" text={<LinkedIn sx={{ fontSize: '2rem' }} />} />
-        <CustomLink href="https://www.facebook.com/nyuad.cities/" text={<Facebook sx={{ fontSize: '2rem' }} />} />
-        <CustomLink href="https://www.instagram.com/cities.nyuad/" text={<Instagram sx={{ fontSize: '2rem' }} />} />
+      <Stack
+        direction="row"
+        spacing={1}
+        justifyContent="center"
+        sx={{
+          '& svg': {
+            fontSize: '2rem'
+          }
+        }}
+      >
+        <CustomLink href="https://twitter.com/cities_nyuad/" text={<Twitter />} />
+        <CustomLink href="https://www.linkedin.com/company/center-for-interacting-urban-networks/" text={<LinkedIn />} />
+        <CustomLink href="https://www.facebook.com/nyuad.cities/" text={<Facebook />} />
+        <CustomLink href="https://www.instagram.com/cities.nyuad/" text={<AiFillInstagram />} />
       </Stack>
 
       <CustomLink href="mailto:nyuad.cities@nyu.edu" text="nyuad.cities@nyu.edu" />
