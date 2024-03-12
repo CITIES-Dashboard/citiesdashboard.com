ab81d59f88fae554a08fbd55408097b1abeb69ba
Ritin Malhotra <rm5486@nyu.edu>
2024-03-13 03:01:37 +0530

    feat: ESLint fix + documentation for `ExpandableSection.jsx`

--- frontend/src/Pages/Project/ExpandableSection.jsx
+++ frontend/src/Pages/Project/ExpandableSection.jsx
@@ -1,6 +1,3 @@
-// disable eslint for this file
-/* eslint-disable */
-
 import { useState } from 'react';
 import { styled } from '@mui/material/styles';
 
@@ -13,56 +10,57 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import * as Tracking from '../../Utils/Tracking';
 
 const StyledAccordion = styled(MuiAccordion)(({ theme, expanded }) => ({
-    color: theme.palette.text.secondary,
-    backgroundImage: 'none',
-    backgroundColor: expanded ? theme.palette.background.default : 'transparent',
-    boxShadow: expanded ? theme.shadows : 'none',
-    transition: 'none',
+  color: theme.palette.text.secondary,
+  backgroundImage: 'none',
+  backgroundColor: expanded ? theme.palette.background.default : 'transparent',
+  boxShadow: expanded ? theme.shadows : 'none',
+  transition: 'none',
 }));
 
 const StyledAccordionSummary = styled(MuiAccordionSummary)(({ theme, expanded }) => ({
-    marginTop: expanded ? theme.spacing(3) : 0,
-    flexDirection: 'row-reverse',
-    paddingLeft: expanded ? theme.spacing(1) : 0,
-    '& .MuiTypography-root': {
-        color: theme.palette.text.primary,
-        textDecoration: 'underline',
-        transition: 'none',
-        '&:hover': {
-            color: theme.palette.primary.main,
-        },
+  marginTop: expanded ? theme.spacing(3) : 0,
+  flexDirection: 'row-reverse',
+  paddingLeft: expanded ? theme.spacing(1) : 0,
+  '& .MuiTypography-root': {
+    color: theme.palette.text.primary,
+    textDecoration: 'underline',
+    transition: 'none',
+    '&:hover': {
+      color: theme.palette.primary.main,
     },
+  },
 }));
 
-const ExpandableSection = ({ title, content }) => {
-    const [expanded, setExpanded] = useState(undefined);
+function ExpandableSection({ title, content }) {
+  const [expanded, setExpanded] = useState(undefined);
 
-    const handleAccordionChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : undefined);
+  const handleAccordionChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : undefined);
 
-        Tracking.sendEventAnalytics(isExpanded ? Tracking.Events.expandSampleData : Tracking.Events.collapseSampleData);
-    };
+    // eslint-disable-next-line max-len
+    Tracking.sendEventAnalytics(isExpanded ? Tracking.Events.expandSampleData : Tracking.Events.collapseSampleData);
+  };
 
-    return (
-        <Box>
-            <StyledAccordion
-                expanded={expanded === "panel1"}
-                onChange={handleAccordionChange("panel1")}
-                transitionDuration={0}
-            >
-                <StyledAccordionSummary expanded={expanded} expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
-                    <Tooltip title={expanded ? "Click to collapse" : "Click to expand"} enterDelay={0} leaveDelay={200}>
-                        <Typography component="a" variant="body1">
-                            {title}
-                        </Typography>
-                    </Tooltip>
-                </StyledAccordionSummary>
-                <MuiAccordionDetails>
-                    {content}
-                </MuiAccordionDetails>
-            </StyledAccordion>
-        </Box>
-    );
+  return (
+    <Box>
+      <StyledAccordion
+        expanded={expanded === 'panel1'}
+        onChange={handleAccordionChange('panel1')}
+        transitionDuration={0}
+      >
+        <StyledAccordionSummary expanded={expanded} expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+          <Tooltip title={expanded ? 'Click to collapse' : 'Click to expand'} enterDelay={0} leaveDelay={200}>
+            <Typography component="a" variant="body1">
+              {title}
+            </Typography>
+          </Tooltip>
+        </StyledAccordionSummary>
+        <MuiAccordionDetails>
+          {content}
+        </MuiAccordionDetails>
+      </StyledAccordion>
+    </Box>
+  );
 }
 
 export default ExpandableSection;
